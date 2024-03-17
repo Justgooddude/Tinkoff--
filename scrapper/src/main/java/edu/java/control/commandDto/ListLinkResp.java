3a17367b4482033b57ef275ce9f3305287941d54
Ivan Gr <drivergood.warchief@gmail.com>
2024-03-18 02:50:47 +0700

    .

--- scrapper/src/main/java/edu/java/control/commandDto/ListLinkResp.java
+++ scrapper/src/main/java/edu/java/control/commandDto/ListLinkResp.java
@@ -1,14 +1,26 @@
 package edu.java.control.commandDto;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
+@Getter
 public class ListLinkResp {
-    public List<LinkResp> links;
-    public int size;
+    private final List<LinkResp> links = new ArrayList<>();
+    public void setLinks(List<String> urls, Long id) {
+        for (String s : urls) {
+            LinkResp linkResponse = new LinkResp(id, URI.create(s));
+            links.add(linkResponse);
+        }
+    }
+    public int getSize() {
+        return links.size();
+    }
 }
